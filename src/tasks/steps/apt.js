7a4f31047be20bdbb98acc9a3332afa6cedc4035
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-04-03 15:50:33 -0300

    Fix front-end question on upgrade

--- src/tasks/steps/apt.js
+++ src/tasks/steps/apt.js
@@ -4,8 +4,9 @@ module.exports = () => [
 
    `echo "${sh.startTitle}Preparing the Environment${sh.endTitle}"`,
    'apt update',
-   'apt upgrade -y',
+   'DEBIAN_FRONTEND="noninteractive" apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade',
    'apt autoremove -y --purge',
+   'apt clean',
    'apt autoclean',
    sh.done,
 ];
