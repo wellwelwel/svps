7f17247f6d6f04e613e10aab6c2b09f0f1e9921d
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-03-21 10:56:57 -0300

    Adding CI tests

---
+++ .svpsrc.example.js
@@ -0,0 +1,82 @@
+/**
+ * Using a Docker Container to create a local VPS:
+ * docker run -d --privileged -p 22:22 --restart always wellwelwel/vps:latest
+ *
+ * If you want test with Remote Desktop, changing `steps.desktop` to `true`:
+ * docker run -d --privileged -p 22:22 -p 3389:3389 --restart always wellwelwel/vps:latest
+ *
+ * Or just put your own VPS access to test
+ */
+
+// @ts-check
+import { defineConfig } from 'svps';
+
+export default defineConfig({
+   access: [
+      {
+         host: '127.0.0.1',
+         username: 'root',
+         password: 'root',
+      },
+   ],
+   users: [
+      {
+         name: 'support',
+         password: '123',
+         sudo: true,
+         sftp: true,
+      },
+      {
+         name: 'manager',
+         password: '456',
+         sftp: {
+            chRoot: '/var/www',
+            chUser: '/var/www/domains',
+         },
+         groups: ['www-data'],
+      },
+   ],
+   apache: {},
+   node: {
+      version: 18,
+      packages: ['yarn'],
+   },
+   mysql: {
+      root: {
+         pass: 'root',
+      },
+      databases: ['mydb'],
+      users: [{ host: '127.0.0.1', name: 'manager', pass: '789' }],
+   },
+   certificate: {
+      days: 365 * 3,
+      fields: {
+         commonName: 'My VPS',
+         country: 'Brasil',
+         location: 'São Paulo',
+         state: 'São Paulo',
+         organization: 'Open Source',
+         organizationUnit: 'Development',
+      },
+   },
+   php: {
+      compose: true,
+      version: 8.2,
+   },
+   steps: {
+      repare: true,
+      apt: true,
+      firewall: true,
+      users: true,
+      certificate: true,
+      apache: true,
+      php: true,
+      node: true,
+      mysql: true,
+      crontab: true,
+      desktop: false,
+      appendCommands: true,
+      reboot: true,
+   },
+   appendCommands: () => ['echo "(„• ᴗ •„)"'],
+});
