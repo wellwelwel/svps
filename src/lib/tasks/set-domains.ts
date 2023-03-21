08f1d1903f680143a8e7b3967de7beee3c2a9c82
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-03-21 15:26:08 -0300

    no-ci: Fix VH conflict when reload Apache

--- src/lib/tasks/set-domains.ts
+++ src/lib/tasks/set-domains.ts
@@ -5,96 +5,96 @@ import { __dirname } from '../modules/root.js';
 import escapeQuotes from '../modules/escape-quotes.js';
 import sh from '../modules/sh.js';
 import { access } from '../modules/configs/access.js';
-import { apache } from '../modules/configs/apache.js';
+import { apache as APACHE } from '../modules/configs/apache.js';
 import { domains as domainsFile } from '../modules/configs/domains.js';
 import { verbose } from '../modules/configs/verbose.js';
+import { APACHE as I_APACHE } from '../types/apache.js';
 
-if (apache) {
-   console.log(`${sh.startTitle}Setting up domains in "${domainsFile}"${sh.endTitle}\n`);
+console.log(`${sh.startTitle}Setting up domains in "${domainsFile}"${sh.endTitle}\n`);
 
-   const fileNormalize = (path: string) => fs.readFileSync(normalize(path), 'utf-8');
+const apache = APACHE as Required<I_APACHE>;
+const fileNormalize = (path: string) => fs.readFileSync(normalize(path), 'utf-8');
 
-   try {
-      const vh_path = '/resources/apache/virtual-host/';
-      const list_domains: string[] = JSON.parse(fs.readFileSync(domainsFile, 'utf-8'));
+try {
+   const vh_path = '/resources/apache/virtual-host/';
+   const list_domains: string[] = JSON.parse(fs.readFileSync(domainsFile, 'utf-8'));
 
-      if (!list_domains || list_domains.length <= 0) throw `Failed to find ${domainsFile}`;
+   if (!list_domains || list_domains.length <= 0) throw `Failed to find ${domainsFile}`;
 
-      const domains = [...new Set(list_domains)];
-      const commands: string[] = [];
+   const domains = [...new Set(list_domains)];
+   const commands: string[] = [];
 
-      for (const full_domain of domains) {
-         const split_domain = full_domain.split(':');
-         const domain = split_domain[0] || full_domain;
-         const isProxy = /:/gm.test(full_domain);
-         const port = isProxy ? split_domain[1] : '';
-         const default_file = fileNormalize(apache.defaultPage);
+   for (const full_domain of domains) {
+      const split_domain = full_domain.split(':');
+      const domain = split_domain[0] || full_domain;
+      const isProxy = /:/gm.test(full_domain);
+      const port = isProxy ? split_domain[1] : '';
+      const default_file = fileNormalize(apache.defaultPage);
 
-         const virtual_host = () =>
-            fileNormalize(`${__dirname}${vh_path}${isProxy ? 'proxy.conf' : 'vh.conf'}`)
+      const virtual_host = () =>
+         fileNormalize(`${__dirname}${vh_path}${isProxy ? 'proxy.conf' : 'vh.conf'}`)
+            .replace(/{!DOMAIN}/gm, domain)
+            .replace(/{!PORT}/gm, port);
+
+      Object.assign(commands, [
+         ...commands,
+         `if ! ls /var/www/${domain}/public_html &> /dev/null; then mkdir -p /var/www/${domain}/public_html; fi`,
+         `if ! ls /var/www/${domain}/public_html/${basename(apache.defaultPage)} &> /dev/null; then echo ${escapeQuotes(
+            default_file
+         )} | cat > /var/www/${domain}/public_html/${basename(apache.defaultPage)}; fi`,
+         `if ! ls /etc/apache2/sites-available/${domain}.conf &> /dev/null; then echo ${escapeQuotes(
+            virtual_host()
+         )} | cat > /etc/apache2/sites-available/${domain}.conf; fi`,
+         `if a2ensite -q ${domain}; then echo "\x1b[33m> \x1b[0m${domain} already enabled"; fi`,
+      ]);
+
+      if (apache.www) {
+         const virtual_host_www = () =>
+            fileNormalize(`${__dirname}${vh_path}${isProxy ? 'proxy-www.conf' : 'vh-www.conf'}`)
                .replace(/{!DOMAIN}/gm, domain)
                .replace(/{!PORT}/gm, port);
 
          Object.assign(commands, [
             ...commands,
-            `if ! ls /var/www/${domain}/public_html &> /dev/null; then mkdir -p /var/www/${domain}/public_html; fi`,
-            `if ! ls /var/www/${domain}/public_html/${basename(
-               apache.defaultPage
-            )} &> /dev/null; then echo ${escapeQuotes(default_file)} | cat > /var/www/${domain}/public_html/${basename(
-               apache.defaultPage
-            )}; fi`,
-            `if ! ls /etc/apache2/sites-available/${domain}.conf &> /dev/null; then echo ${escapeQuotes(
-               virtual_host()
-            )} | cat > /etc/apache2/sites-available/${domain}.conf; fi`,
-            `if a2ensite -q ${domain}; then echo "\x1b[33m> \x1b[0m${domain} already enabled"; fi`,
-         ]);
-
-         if (apache.www) {
-            const virtual_host_www = () =>
-               fileNormalize(`${__dirname}${vh_path}${isProxy ? 'proxy-www.conf' : 'vh-www.conf'}`)
-                  .replace(/{!DOMAIN}/gm, domain)
-                  .replace(/{!PORT}/gm, port);
-
-            Object.assign(commands, [
-               ...commands,
-               `if ! ls /etc/apache2/sites-available/www.${domain}.conf &> /dev/null; then echo ${escapeQuotes(
-                  virtual_host_www()
-               )} | cat > /etc/apache2/sites-available/www.${domain}.conf; fi`,
-               `if a2ensite -q www.${domain}; then echo "\x1b[33m> \x1b[0mwww.${domain} already enabled"; fi`,
-            ]);
-         }
-
-         if (!isProxy) continue;
-
-         /* Creates app.js */
-         const app_js = fileNormalize(`${__dirname}/resources/node/app.js`).replace(/'{!PORT}'/gm, port);
-
-         Object.assign(commands, [
-            ...commands,
-            `if ! ls /var/www/${domain}/app.js &> /dev/null; then echo ${escapeQuotes(
-               app_js
-            )} | cat > /var/www/${domain}/app.js && (pm2 delete ${domain} || true) && pm2 start /var/www/${domain}/app.js --name ${domain} --watch --ignore-watch="node_modules"; fi`,
-            'pm2 save',
+            `if ! ls /etc/apache2/sites-available/www.${domain}.conf &> /dev/null; then echo ${escapeQuotes(
+               virtual_host_www()
+            )} | cat > /etc/apache2/sites-available/www.${domain}.conf; fi`,
+            `if a2ensite -q www.${domain}; then echo "\x1b[33m> \x1b[0mwww.${domain} already enabled"; fi`,
          ]);
       }
 
       commands.push('systemctl reload apache2');
 
-      if (verbose) console.log(commands, '\n');
+      if (!isProxy) continue;
 
-      const hosts = access;
+      /* Creates app.js */
+      const app_js = fileNormalize(`${__dirname}/resources/node/app.js`).replace(/'{!PORT}'/gm, port);
 
-      for (const host of hosts) {
-         await connect(host);
-         for (const command of commands) await exec(command, host);
-         await exec('history -c', host);
-         await end();
-      }
+      Object.assign(commands, [
+         ...commands,
+         `if ! ls /var/www/${domain}/app.js &> /dev/null; then echo ${escapeQuotes(
+            app_js
+         )} | cat > /var/www/${domain}/app.js && (pm2 delete ${domain} &> /dev/null || true) && pm2 start -f /var/www/${domain}/app.js --name ${domain} --watch --ignore-watch="node_modules"; fi`,
+         'pm2 save',
+      ]);
 
-      console.log(`\x1b[0m\x1b[1m$'\x1b[32m✔︎ Success'\x1b[0m\n`);
-      process.exit(0);
-   } catch (error) {
-      console.log(`\x1b[0m\x1b[1m\n\x1b[31m✖︎ Fail\x1b[0m\n  ${error}`);
-      process.exit(1);
+      commands.push('pm2 update');
    }
+
+   if (verbose) console.log(commands, '\n');
+
+   const hosts = access;
+
+   for (const host of hosts) {
+      await connect(host);
+      for (const command of commands) await exec(command, host);
+      await exec('history -c', host);
+      await end();
+   }
+
+   console.log(`\x1b[0m\x1b[1m\x1b[32m✔︎ Success\x1b[0m\n`);
+   process.exit(0);
+} catch (error) {
+   console.log(`\x1b[0m\x1b[1m\n\x1b[31m✖︎ Fail\x1b[0m\n  ${error}`);
+   process.exit(1);
 }
