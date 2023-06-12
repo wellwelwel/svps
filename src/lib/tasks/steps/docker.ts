cbe41441584f97b9469de18b738a9dfa621c344c
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2023-06-11 23:36:14 -0300

    Fixing docker installation

--- src/lib/tasks/steps/docker.ts
+++ src/lib/tasks/steps/docker.ts
@@ -14,6 +14,7 @@ export default () => {
       'echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null',
       'apt-get update',
       'apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y',
+      'systemctl start docker > /dev/null',
       sh.done,
    ];
 };
